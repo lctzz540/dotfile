b58f8d1e1f9082e54acf5d43fe1a340832f313fb
lctzz540 <lctzz540@gmail.com>
2022-06-11 00:03:37 +0700

    first commit

---
+++ fish/config.fish
@@ -0,0 +1,22 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -U fish_greeting ""
+
+    alias btrfortune='fortune -a -s | head -n 1 | figlet | lolcat'
+    alias fortunecow='fortune -a -s | head -n 1 | cowsay | lolcat'
+    
+    alias l='exa -lahF --color=always --icons --sort=size --group-directories-first'
+    alias ls='exa -lhF --color=always --icons --sort=size --group-directories-first'
+    alias lst='exa -lahFT --color=always --icons --sort=size --group-directories-first'
+    
+    alias matrix='unimatrix -f -l ocCgGkS -s 96'
+    alias clock='tty-clock -sct -C 4'
+    alias pipes='pipes -p 5 -R -t 1 -r 0'
+    colorscript -r
+end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/w/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
---
+++ fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,5 @@
+bind \cx 'exit'
+bind \cn 'nvim; commandline -f repaint'
+bind \co 'ranger; commandline -f repaint'
+bind \cr 'reset; source ~/.config/fish/config.fish; commandline -f repaint'
+bind \cg 'git pull && git status; git diff; echo "\n" && commandline -f repaint'
